5f42f6a3c14b316eac62960afdb484db11af4c62
Morgan Franke <morganleighfranke@gmail.com>
2021-02-06 13:09:24 -0700

    psuedo code and html done

---
+++ assets/JS/script.js
@@ -0,0 +1,38 @@
+//when i enter the homepage, I have the drop menu for each state
+//an event listener for dropdown that populates a list for each state
+//when i click on the state I want then i get a list of every park within my state
+//populate the list with the info you get from the API, likely state and park as queries
+//the park names become links to a new page that gives me the weather for that location and a picture and a link to the NPS park site
+//we need the weather api to find the location of the chosen park and populate that data into the weatherBox on the html... somehow mapquest helps with this
+//stored locally are 'Your recent adventures' which are the parks you looked at already
+//at the bottom of the page you will always see 'Your recent adventures' to help you plan your vacation, recents populates of the picture of that park you looked at
+
+//image carousel, not finished
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+
+// API's
+//Open weater
+//nps
+//mapquest
+
+
+
+
+
+//
