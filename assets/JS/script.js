f0e3dc6d6e55805a5d7ee887373307e1888b6b70
Tolga Secme <tolgasecme@icloud.com>
2021-02-10 20:36:01 -0700

    Weather app fixed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,4 @@
+
 //when i enter the homepage, I have the drop menu for each state
 //an event listener for dropdown that populates a list for each state
 //when i click on the state I want then i get a list of every park within my state
@@ -39,6 +40,9 @@ function trailFind() {
       /* console.log(data);
       console.log(data.data); */
       $("#parks").empty();
+      $("#fetch-weather").on("click", function () {
+        weatherLatLon();
+      })
       for (let i = 0; i < data.data.length; i++) {
         /* console.log(data.data.length); */
         latitude = data.data[i].latitude;
@@ -48,6 +52,7 @@ function trailFind() {
 
         /* console.log(data.data.length); */
         let response = data.data[i];
+        /* localStorage.setItem(`nationalName-${i}`, response.fullName); */
         /* console.log(response.fullName); */
         /* let card = document.getElementById("#") */
         let divCell = $("<div>").addClass("cell");
@@ -64,27 +69,25 @@ function trailFind() {
           //
         }
         let divCardSection = $("<div>").addClass("card-section");
-        let pTag = $("<h4>").text(response.fullName);
+        let pTag = $("<a>").text(response.fullName);
 
         divCell.append(card);
         card.append(imgSrc, divCardSection);
         divCardSection.append(pTag);
         $("#parks").append(divCell);
-
         /* let divCardContent = $("<div>").addClass("card-content");
         let cardBodyDiv = $("<div>").addClass("card-body");
         let mediaClass = $("<div>").addClass("media");
         let mediaContent = $("<div>").addClass("media-content"); */
-
-
-
-        weatherLatLon();
       }
     })
 }
 
+
+
 $("#SubmitBtn").on("click", function () {
   state = $("#given-input").val().trim();
+  $("#fetch-weather").css('display', 'block');
   trailFind();
 })
 
@@ -97,19 +100,37 @@ $("#SubmitBtn").on("click", function () {
 
 //wherever call function weatherLatLon need to put in arguments of latitude longitude 
 function weatherLatLon() {
-  const requestWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lon=" + longitude + "&lat=" + latitude + "&appid=ca7c03ab6ebfee5c7d96f4deeccbecc0";
+  const requestWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lon=" + longitude + "&lat=" + latitude + "&units=imperial&appid=ca7c03ab6ebfee5c7d96f4deeccbecc0";
 
   fetch(requestWeatherUrl)
     .then(function (data) {
       return data.json();
     }).then(function (data) {
       console.log(data);
-    })
+      for (let i = 0; i < 40; i += 8) {
+        let days = data.list[i];
+        /* console.log(data.list[0].dt_txt); */
 
-  //call long and lat from nps object that is returned from fetch
-  //within that object we need to grab the lat and long individually and pass them into the variables of longitude and latitude
-  //
+        let cardInit = $("<div>").addClass("column column-block whole");
+        let cardDay = $("<div>").text(days.dt_txt.slice(0, 10));
+        /* console.log(data.list[0].main.temp + "˚F"); */
+        let degree = $("<p>").text(Math.round(days.main.temp) + "˚F");
+        /* console.log(data.list[0].main.humidity + " %"); */
+        let humid = $("<p>").text("Humidity: " + days.main.humidity + "%");
+        /* console.log(data.list[0].wind.speed + " mph"); */
+        let wind = $("<p>").text("wind Speed: " + Math.round(days.wind.speed) + " mph");
+        let icon = $("<img>");
+        icon.attr("src", "http://openweathermap.org/img/wn/" + days.weather[0].icon + "@2x.png");
+        $("#fetch-five").append(cardInit.append(cardDay, degree, icon, humid, wind));
+      }
+
+    })
 }
+      //call long and lat from nps object that is returned from fetch
+      //within that object we need to grab the lat and long individually and pass them into the variables of longitude and latitude
+      //
+/* localStorage.setItem("Weather-in-Park",) */
+
 
 
 //we need the weather api to find the location of the chosen park and populate that data into the weatherBox on the html... somehow mapquest helps with this
@@ -118,5 +139,3 @@ function weatherLatLon() {
 
 //stored locally are 'Your recent adventures' which are the parks you looked at already
 //at the bottom of the page you will always see 'Your recent adventures' to help you plan your vacation, recents populates of the picture of that park you looked at
-
-
